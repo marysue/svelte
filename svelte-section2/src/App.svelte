b256f7e6aa0714d5b94865c802f52a56e243d8fe
Mary Lark <mary@marylark.com>
2024-01-27 13:31:27 -0700

    Section 2 added

---
+++ svelte-section2/src/App.svelte
@@ -0,0 +1,15 @@
+<!-- App.svelt -->
+<script>
+	import Counter from './lib/Counter.svelte';
+	const maxCount = 10;
+
+	let props = {
+		initialCount: 3,
+		maxCount
+	};
+</script>
+
+<Counter {...props} x="2" />
+
+<style>
+</style>
---
+++ svelte-section2/svelte-course/src/App.svelte
@@ -0,0 +1,19 @@
+<!-- App.svelt -->
+<script>
+	import Counter from './lib/Counter.svelte';
+	const maxCount = 10;
+
+	let props = {
+		initialCount: 3,
+		maxCount
+	};
+</script>
+
+<!-- Demonstrates spreading props and passing in an
+     unexpected prop of x not declared in your export
+     declarations
+-->
+<Counter {...props} x="2" />
+
+<style>
+</style>
